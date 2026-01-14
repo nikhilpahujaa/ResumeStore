3a4e30b8e1fc8fd620f3c2b67a7edfca20950c84
Nikhil Pahuja <nikhilpahuja75@gmail.com>
2026-01-14 21:09:37 +0530

    updated resume

--- src/data/resumes.js
+++ src/data/resumes.js
@@ -1,16 +1,16 @@
 export const resumes = [
   {
     id: 1,
-    title: "Resume Version A",
+    title: "Resume A",
     pdfUrl: "/resumes/A.pdf",
     previewImageUrl: "/previews/A-preview.png",
-    downloadName: "Nikhil's_resume_A.pdf"
+    downloadName: "Nikhil's_resume.pdf"
   },
   {
     id: 2,
-    title: "Resume Version B",
+    title: "Resume B",
     pdfUrl: "/resumes/B.pdf",
     previewImageUrl: "/previews/B-preview.png",
-    downloadName: "Nikhil's_resume_B.pdf"
+    downloadName: "Nikhil's_resume.pdf"
   }
 ];
